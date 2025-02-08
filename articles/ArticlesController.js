a936470f9d866b118fb1c9eb3aacf38dfb942608
Bernarda Gonçalves <ireneteresa.13@gmail.com>
2025-02-08 21:05:41 +0100

    Testando o commit

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -3,6 +3,7 @@ const router = express.Router()
 const Category = require ("../categories/Category")
 const Article = require ("./Article")
 const slugify = require("slugify")
+const { where } = require("sequelize")
 
 router.get("/admin/articles", (req, res) => {
     Article.findAll({
@@ -54,19 +55,69 @@ router.post("/articles/delete", (req, res) => {
 
 router.get("/admin/articles/edit/:id", (req, res) => {
     var id = req.params.id
-    Article.findByPk(id).then(article =>{
-        if(article!= undefined){
 
+    if (isNaN(id)){
+        res.redirect("/admin/articles")
+    }
+    Article.findByPk(id).then(article => {
+        if(article != undefined){
             Category.findAll().then(categories =>{
-                res.render("admin/articles/edit", {categories: categories})
+                res.render("admin/articles/edit", {categories: categories, article: article})
             })
-
-        } else[
+        } else{
             res.redirect("/")
-        ]
+        }
     }).catch(error => {
         res.redirect("/")
     });
 })
 
+router.post("/articles/update", (req, res) => {
+    var id = req.body.id
+    var title = req.body.title
+    var body = req.body.body
+    var category = req.body.category
+
+    Article.update({title: title, body: body, categoryId: category, slug: slugify(title)}, {
+        where: {
+            id: id
+        }
+    }).then(() => {
+        res.redirect("/admin/articles")
+    }).catch(error => {
+        res.redirect("/")
+    })
+})
+
+// Paginacao dos artigos
+router.get("/articles/page/:num", (req, res) => {
+    var page = req.params.num
+
+    if(isNaN(page) || page == 1){
+        offset = 0
+    }else{
+        offset = parseInt(page) * 4
+    }
+
+    Article.findAndCountAll({
+        limit: 4,
+        offset: offset
+
+    }).then(articles => {
+        var next
+
+        if(offset + 4 >= articles.count){
+            next = false
+        }else {
+            next = true
+        }
+
+        var result = {
+            next: next,
+            articles: articles
+        }
+
+        res.json(articles)
+    })
+})
 module.exports = router
--- database/database.js
+++ database/database.js
@@ -1,6 +1,6 @@
 const Sequelize = require("sequelize")
 
-const connection = new Sequelize('guiapress', 'root', 'Doramaslove#098', {
+const connection = new Sequelize('guiapress', 'root', '', {
     host: 'localhost',
     dialect: 'mysql',
     timezone: "+01:00"
