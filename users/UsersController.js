8b915cedb7c50b7069df9ca62202a9ffd62cb563
Bernarda Gonçalves <ireneteresa.13@gmail.com>
2025-02-09 14:52:02 +0100

    Listagem de Usuários

--- users/UsersController.js
+++ users/UsersController.js
@@ -2,9 +2,13 @@ const express = require("express")
 const router = express.Router()
 const User = require("./User")
 const bcrypt = require("bcryptjs")
+const { where } = require("sequelize")
 
 router.get("/admin/users", (req, res) => {
-    res.send("Listagem de usuários")
+    User.findAll().then(users => {
+        res.render("admin/users/index", {users: users})
+    })
+    //res.send("Listagem de usuários")
 })
 
 router.get("/admin/users/create", (req, res) => {
@@ -15,6 +19,13 @@ router.post("/users/create", (req, res) =>{
     var email = req.body.email
     var password = req.body.password
 
+    User.findOne({
+        where: {
+            email: email
+        }
+    }).then(user => {
+        if(user == undefined){
+            
     var salt = bcrypt.genSaltSync(10)
     var hash = bcrypt.hashSync(password,salt)
 
@@ -26,6 +37,12 @@ router.post("/users/create", (req, res) =>{
     }).catch((error) => {
         res.redirect("/")
     })
+
+        }else{
+            res.redirect("/admin/users/create")
+        }
+    })
+
 
     //res.json({email, password}) teste dos dados
 })
